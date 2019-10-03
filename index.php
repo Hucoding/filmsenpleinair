7ccf556bafd3f0fb2c6162b8fc7eebbdba3b7968
Hugo M <hmuttnpro@gmail.com>
2019-10-03 20:40:07 +0200

    update 1

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Films en plein air - vivez l'expérience !</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+        <style>
+        .fakeimg {
+            height: 200px;
+            background: #aaa;
+        }
+        </style>
+    </head>
+<body>
+<div id="presentation" class="carousel slide carousel-fade" data-ride="carousel" style="margin-bottom:0">
+        <div class="carousel-caption">
+            <h1>Film en plein air</h1>
+            <p>Vivez une expérience inédite</p>
+        </div>
+
+        <!--INDICATOR-->
+            <ul class="carousel-indicators">
+                <li data-target="Images/films-en-plein-air-paris-monceau-1.jpg" data-slide-to="0" class="active"></li>
+                <li data-target="Images/films-en-plein-air-paris-monceau-2.jpg" data-slide-to="1"></li>
+                <li data-target="Images/films-en-plein-air-paris-monceau-3.jpg" data-slide-to="2"></li>
+            </ul>
+
+        <!--SLIDE-->
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="Images/films-en-plein-air-paris-monceau-1.jpg" class="d-block w-100" alt="affiche-films1" style="height: 650px;">
+            </div>
+            <div class="carousel-item">
+                <img src="Images/films-en-plein-air-paris-monceau-2.jpg" class="d-block w-100" alt="affiche-film2" style="height: 650px;">
+            </div>
+            <div class="carousel-item">
+                <img src="Images/films-en-plein-air-paris-monceau-3.jpg" class="d-block w-100" alt="affiche-film3" style="height: 650px;">
+            </div>
+        </div>
+
+        <!-- Left and right controls -->
+        <a class="carousel-control-prev" href="#presentation" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+        </a>
+        <a class="carousel-control-next" href="#presentation" role="button" data-slide="next">
+            <span class="carousel-control-next-icon"></span>
+        </a>
+
+    </div>
+        <nav class="navbar navbar-expand-sm bg-dark justify-content-center navbar-dark">
+            <a class="navbar-brand" href="#"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            <div class="collapse-center navbar-collapse-center" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">À l'affiche</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Réservation</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Nous contacter</a>
+                </li>    
+                </ul>
+            </div>  
+        </nav>
+        <!--PRINCIPAL CONTAINER-->
+                    <div class="container" style="margin-top:30px; margin-left: 20px;">
+                        <div class="col-md-6">
+                            <img src="Images/films-en-plein-air-paris-monceau-2.jpg" style=" height:300px; width:250px; border-radius: 10px; padding: 10px;" alt="...">
+                            <div class="media-body">
+                                <h5 class="mt-0">Film de la semaine</h5>
+                                <p>Cras sit amet nibh libero, in gravida nsmùldmùlùulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+                                <a href="#" class="stretched-link">Go somewhere</a>
+                            </div>
+                        </div>
+                            <br>
+                        <div class="col-md-6">
+                            <img src="Images/films-en-plein-air-paris-monceau-2.jpg" style=" height:300px; width:250px; border-radius: 10px; padding: 10px;" alt="...">
+                            <div class="media-body">    
+                                <h5 class="mt-0">Film de la semaine</h5>
+                                <p>Cras sit amet nibh libero, in gravida nsmùldmùlùulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+                                <a href="#" class="stretched-link">Go somewhere</a>
+                            </div>
+                        </div>
+                            <br>
+                        <div class="col-md-6">
+                            <img src="Images/films-en-plein-air-paris-monceau-2.jpg" style=" height:300px; width:250px; border-radius: 10px; padding: 10px;" alt="...">
+                            <div class="media-body">   
+                                <h5 class="mt-0">Film de la semaine</h5>
+                                <p>Cras sit amet nibh libero, in gravida nsmùldmùlùulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+                                <a href="#" class="stretched-link">Go somewhere</a>
+                            </div>
+                        </div>
+    
+                <!--div class="col-sm-8">
+                        <h2>TITLE HEADING</h2>
+                        <h5>Title description, Dec 7, 2017</h5>
+                    <div class="fakeimg">Fake Image</div>
+                        <p>Some text..</p>
+                        <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                    <br>
+                    <h2>TITLE HEADING</h2>
+                    <h5>Title description, Sep 2, 2017</h5>
+                        <div class="fakeimg">Fake Image</div>
+                            <p>Some text..</p>
+                            <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                    <br>
+                </div-->
+                <!--ASIDE RIGHT-->
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h2>About Me</h2>
+                            <h5>Photo of me:</h5>
+                            <div class="fakeimg">Fake Image</div>
+                                <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+                                <h3>Some Links</h3>
+                                <p>Lorem ipsum dolor sit ame.</p>
+                                <ul class="nav nav-pills flex-column">
+                                    <li class="nav-item">
+                                        <a class="nav-link active" href="#">Active</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Link</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Link</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link disabled" href="#">Disabled</a>
+                                    </li>
+                                </ul>
+                            <hr class="d-sm-none">
+                        </div>
+                    </div>
+                </div>
+        <!--FOOTER-->
+        <div class="jumbotron text-center" style="margin-bottom:0">
+        <p>Footer</p>
+        </div>
+</body>
+</html>
