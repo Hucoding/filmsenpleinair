3e7047f800d820ee3f1c0e8765bff1a00d7ca0d7
Hugo M <hmuttnpro@gmail.com>
2019-10-06 20:02:26 +0200

    update parallax

--- index.php
+++ index.php
@@ -7,18 +7,15 @@
         <title>Films en plein air - vivez l'expérience !</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-
+        <link rel="stylesheet" href="css/parallax.css"/>
         <style>
-        .fakeimg {
-            height: 200px;
-            background: #aaa;
+        #reservation{
+            background-image: url("../../media/examples/lizard.png");
         }
         </style>
     </head>
 <body>
+
 <div id="presentation" class="carousel slide carousel-fade" data-ride="carousel" style="margin-bottom:0">
         <div class="carousel-caption">
             <h1>Film en plein air</h1>
@@ -54,6 +51,7 @@
         </a>
 
     </div>
+   
         <nav class="navbar navbar-expand-sm bg-dark justify-content-center navbar-dark">
             <a class="navbar-brand" href="#"></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
@@ -77,78 +75,117 @@
             </div>  
         </nav>
         <!--PRINCIPAL CONTAINER-->
-                    <div class="container" style="margin-top:30px; margin-left: 20px;">
-                        <div class="col-md-6">
-                            <img src="Images/films-en-plein-air-paris-monceau-2.jpg" style=" height:300px; width:250px; border-radius: 10px; padding: 10px;" alt="...">
-                            <div class="media-body">
-                                <h5 class="mt-0">Film de la semaine</h5>
-                                <p>Cras sit amet nibh libero, in gravida nsmùldmùlùulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-                                <a href="#" class="stretched-link">Go somewhere</a>
-                            </div>
-                        </div>
-                            <br>
-                        <div class="col-md-6">
-                            <img src="Images/films-en-plein-air-paris-monceau-2.jpg" style=" height:300px; width:250px; border-radius: 10px; padding: 10px;" alt="...">
-                            <div class="media-body">    
-                                <h5 class="mt-0">Film de la semaine</h5>
-                                <p>Cras sit amet nibh libero, in gravida nsmùldmùlùulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-                                <a href="#" class="stretched-link">Go somewhere</a>
-                            </div>
-                        </div>
-                            <br>
-                        <div class="col-md-6">
-                            <img src="Images/films-en-plein-air-paris-monceau-2.jpg" style=" height:300px; width:250px; border-radius: 10px; padding: 10px;" alt="...">
-                            <div class="media-body">   
-                                <h5 class="mt-0">Film de la semaine</h5>
-                                <p>Cras sit amet nibh libero, in gravida nsmùldmùlùulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-                                <a href="#" class="stretched-link">Go somewhere</a>
-                            </div>
-                        </div>
-    
-                <!--div class="col-sm-8">
-                        <h2>TITLE HEADING</h2>
-                        <h5>Title description, Dec 7, 2017</h5>
-                    <div class="fakeimg">Fake Image</div>
-                        <p>Some text..</p>
-                        <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-                    <br>
-                    <h2>TITLE HEADING</h2>
-                    <h5>Title description, Sep 2, 2017</h5>
-                        <div class="fakeimg">Fake Image</div>
-                            <p>Some text..</p>
-                            <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-                    <br>
-                </div-->
-                <!--ASIDE RIGHT-->
-                    <div class="row">
-                        <div class="col-md-6">
-                            <h2>About Me</h2>
-                            <h5>Photo of me:</h5>
-                            <div class="fakeimg">Fake Image</div>
-                                <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
-                                <h3>Some Links</h3>
-                                <p>Lorem ipsum dolor sit ame.</p>
-                                <ul class="nav nav-pills flex-column">
-                                    <li class="nav-item">
-                                        <a class="nav-link active" href="#">Active</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="#">Link</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="#">Link</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link disabled" href="#">Disabled</a>
-                                    </li>
-                                </ul>
-                            <hr class="d-sm-none">
-                        </div>
-                    </div>
-                </div>
-        <!--FOOTER-->
-        <div class="jumbotron text-center" style="margin-bottom:0">
-        <p>Footer</p>
+        <div style="color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify;">
+        <div class="container">
+        
+        <div class="col-sm-12" style="text-align: left; margin-top: 10px; margin-bottom: 50px;">
+                <h2 class="mt-0" style="text-align: center;">A L'AFFICHE</h2>
+                <i><p>Nous avons sélectionner le meilleurs du cinéma pour vous, venez profitez des films en plein air en famille, entre amis, ou en amoureux ! vous pouvez dés maintenant vous inscrire via notre formulaire de réservation.
+                <br>Comment ça marche ? c'est très simple vous devez obligatoirement réservez votre place via le formulaire de réservation, pour que nous puissions prévoir en conséquence.</p></i>
+        </div> 
+    <div class="row">
+        <div class="col-sm-3">
+            <img src="Images/films-en-plein-air-paris-monceau-2.jpg" style=" height:300px; width:250px; border-radius: 10px; padding: 10px;" alt="...">
         </div>
+        <div class="col-sm-7">
+                <h3 class="mt-0">Mr Robot</h3>
+                <p>Cras sit amet nibh libero, in gravida nsmùldmùlùulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+                <a href="#" class="btn btn-primary">En savoir +</a>
+        </div>
+        <div class="col-sm-2">
+            <h4>Casting :</h4>
+            <ul>
+                <li>Blanca Suarez</li>
+            </ul>
+        </div>
+        <div class="col-sm-3">
+            <img src="Images/films-en-plein-air-paris-monceau-2.jpg" style=" height:300px; width:250px; border-radius: 10px; padding: 10px;" alt="...">
+        </div>
+        <div class="col-sm-7">
+                <h3 class="mt-0">Les demoiselles du téléphone</h3>
+                <p>Cras sit amet nibh libero, in gravida nsmùldmùlùulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+                <a href="#" class="btn btn-primary">En savoir +</a>
+        </div>
+        <div class="col-sm-2">
+            <h4>Casting :</h4>
+            <ul>
+                <li>Blanca Suarez</li>
+            </ul>
+        </div>
+        <div class="col-sm-3">
+            <img src="Images/films-en-plein-air-paris-monceau-2.jpg" style=" height:300px; width:250px; border-radius: 10px; padding: 10px;" alt="...">
+        </div>
+        <div class="col-sm-7">
+                <h3 class="mt-0">Les demoiselles du téléphone</h3>
+                <p>Cras sit amet nibh libero, in gravida nsmùldmùlùulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+                <a href="#" class="btn btn-primary">En savoir +</a>
+        </div>
+        <div class="col-sm-2">
+            <h4>Casting :</h4>
+            <ul>
+                <li>Blanca Suarez</li>
+            </ul>
+        </div>
+        <div class="col-sm-3">
+            <img src="Images/films-en-plein-air-paris-monceau-2.jpg" style=" height:300px; width:250px; border-radius: 10px; padding: 10px;" alt="...">
+        </div>
+        <div class="col-sm-7">
+                <h3 class="mt-0">Les demoiselles du téléphone</h3>
+                <p>Cras sit amet nibh libero, in gravida nsmùldmùlùulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+                <a href="#" class="btn btn-primary">En savoir +</a>
+        </div>
+        <div class="col-sm-2">
+            <h4>Casting :</h4>
+            <ul>
+                <li>Blanca Suarez</li>
+            </ul>
+        </div>
+    </div>
+</div>
+    </div>
+    <div class="bgimg-2">
+        <div class="caption">
+            <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">RESERVEZ DES MAINTENANT !</span>
+        </div>
+    </div>
+        <div style="color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify;">
+            <h3 class="mt-0" style="text-align: center;">RESERVATION</h3>
+            <p style="text-align: center;">Réserver gratuitement ICI vos places pour le festival du film en plein air !<br></p>
+            
+            <!--SYSTEME DE RESERVATION-->
+            <form action="" method="GET">
+                <fieldset>
+                    <legend>Vos coordonnés:</legend>
+                    <label for="lastname">Nom: </label>
+                        <input type="text" name="lastname" id="lastname" required autofocus placeholder="Votre nom" pattern="[a-zA-Z]{3,}" title="Vous devez entrer plus de 3 caractères pour votre nom.">
+                    <label for="firstname"></label>
+                        <input type="text" name="firstname" id="firstname" required autofocus placeholder="Votre prénom" pattern="[a-zA-Z]{3,}" title="Vous devez entrer plus de 3 caractères pour votre prénom.">
+                    <label for="email"></label>
+                        <input type="text" name="email" id="email" required placeholder="Votre adresse email" pattern="[a-zA-Z]{3,}@[a-zA-Z]{3,}[.]{1}[a-zA-Z]{2,}" title="Vous devez une adresse email valide."> 
+                    <label for="phone">Téléphone: </label>
+                        <input type="tel" name="phone" id="phone" required placeholder="Votre numéro de téléphone" pattern="[0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2}" title="Vous devez entrer un numéro de téléphone sous ce format : ## ## ## ## ##">
+                    <select name="country" required>
+                        <option value=""></option>
+                        <option value=""></option>
+                        <option value=""></option>
+
+                    </select>
+                    </fieldset>
+       
+        </div>
+        <div class="bgimg-2">
+            <div class="caption"> 
+                <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">NOUS CONTACTER</span>
+            </div>
+        </div>
+        <!--FOOTER-->
+    <div class="jumbotron text-center" style="margin-bottom:0">
+        <p>Footer</p>
+    </div>
 </body>
+<footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</footer>
 </html>
