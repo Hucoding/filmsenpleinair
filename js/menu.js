39965ffed8fd41f935d343c0a62d1ed278e1a762
Hugo M <hmuttnpro@gmail.com>
2019-11-04 09:56:47 +0100

    MAJ 04/11/2019

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,15 @@
-$(document).ready(function() {
-    $("[data-toggle]").click(function() {
-            var toggle_el = $(this).data("toggle");
-        $(toggle_el).toggleClass("open-sidebar");
+const fdpa_open_btn = document.querySelector('.fdpa-open-btn');
+
+const fdpa_close_btn = document.querySelector('.fdpa-close-btn');
+
+const fdpa_nav = document.querySelectorAll('.fdpa-nav');
+
+    fdpa_open_btn.addEventListener('click', () => {
+        fdpa_nav.forEach(nav_el => {
+            nav_el.classList.add('visible') });
     });
-});
+
+    fdpa_close_btn.addEventListener('click', () => {
+        fdpa_nav.forEach(nav_el => {
+            nav_el.classList.remove('visible') });
+    });
